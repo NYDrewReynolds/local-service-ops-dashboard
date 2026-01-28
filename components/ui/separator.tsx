74edc33a15f03df390c4927d3e60a586a51f8c02
Drew Reynolds <drew@juniphq.com>
2026-01-28 12:26:40 -0500

    Initialize shadcn UI via CLI
    Regenerate UI components and install Radix deps using the official shadcn CLI.

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,17 +1,28 @@
-import * as React from "react";
+"use client"
 
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
-const Separator = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      role="separator"
-      className={cn("h-px w-full bg-slate-800", className)}
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
       {...props}
     />
   )
-);
-Separator.displayName = "Separator";
+}
 
-export { Separator };
+export { Separator }
